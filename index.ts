da10477ad2a6754605f0f7ee28694e4d6d9c2ea8
lyjelin <u3560393@connect.hku.hk>
2021-06-02 20:55:42 +0800

    #3 Setting Typscrupt Up

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-alert("Hello");
+const name = "elin",
+    age = 24,
+    gender = "male";
+
+
+const sayHi = (name, age, gender) => {
+    console.log("Hello ${name}, you are ${age}, you are a ${gender}")
+};
+
+sayHi(name, age, gender);
+
+export {};
