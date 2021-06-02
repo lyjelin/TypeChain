984e5d712fede162dd54fec1f154fab906f84b0e
lyjelin <u3560393@connect.hku.hk>
2021-06-02 22:00:33 +0800

    #7 Classes on TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+class Human{
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const lynn = new Human("Lynn", 28, "female");
+
 interface Human {
     name: string;
     age: number;
